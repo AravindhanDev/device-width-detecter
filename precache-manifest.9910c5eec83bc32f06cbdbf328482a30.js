9ff6173d11188c4bf872e1996da871c7ae05e91b
noob <noobie37@protonmail.com>
2021-06-11 13:57:27 +0530

    Updates

---
+++ precache-manifest.9910c5eec83bc32f06cbdbf328482a30.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d264c0196981b30d984cbf0aab65aa",
+    "url": "/device-width-detecter/index.html"
+  },
+  {
+    "revision": "558e53fce5af30977421",
+    "url": "/device-width-detecter/static/js/2.1a3e9c21.chunk.js"
+  },
+  {
+    "revision": "8fc91b5abe5e52200941",
+    "url": "/device-width-detecter/static/js/main.76438810.chunk.js"
+  },
+  {
+    "revision": "53e9013a1421f2050370",
+    "url": "/device-width-detecter/static/js/runtime-main.4b06da86.js"
+  }
+]);
